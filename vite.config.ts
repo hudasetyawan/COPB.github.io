31b58c2f0a156341299b0115946131458dc49c5d
hudasetyawan <119984016+hudasetyawan@users.noreply.github.com>
2025-08-29 19:24:10 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // Ganti "COPB.github.io" dengan nama repo kamu
 export default defineConfig({
   plugins: [react()],
-  base: "/COPB.github.io/",
+  base: "/copb.github.io/",
 })
+
